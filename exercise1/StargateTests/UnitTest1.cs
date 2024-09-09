32cafb4ec4b267e3e43a3dc5f587f220c41eed02
Nathan Tymofichuk <nathan.tymofichuk@varent.us>
2024-09-09 08:22:54 -0600

    updated test workflow to use testing project.

--- exercise1/StargateTests/UnitTest1.cs
+++ exercise1/StargateTests/UnitTest1.cs
@@ -18,8 +18,13 @@ namespace StargateTests
         // Important Test cases to cover
 
         // For Person Controller
-        // Making sure that attempting to create a person with a blank or whitespace name returns an error code.
-        // Making sure that attempting to create a person with the same name as another person returns an error.
+        // Attempting to create a person with a blank or whitespace name returns an error code.
+        // Attempting to create a person with the same name as another person returns an error.
+        // Attempting to create a person with a unique name returns success.
+        // Searching by name for a person that doesn't exist returns nothing.
+        // Searching for all people returns all people.
+        // If no people exist searching for all people returns nothing.
+
 
     }
 }
